f1edfad7e8c4a302c9da2bc33d42cdccd09ab9f9
ADMIN <ADMIN@PC218>
2024-06-27 09:27:52 +0700

    Add project files.

---
+++ ExamWeb_NguyenTanHoang/Migrations/20240627020707_InitSchema.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ExamWeb_NguyenTanHoang.Migrations
+{
+    public partial class InitSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DiaNhacs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TieuDe = table.Column<int>(type: "int", maxLength: 250, nullable: false),
+                    NgheSi = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true),
+                    TrongNuoc = table.Column<bool>(type: "bit", nullable: false),
+                    GiaBan = table.Column<double>(type: "float", nullable: false),
+                    SoLuong = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DiaNhacs", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DiaNhacs");
+        }
+    }
+}
---
+++ ExamWeb_NguyenTanHoang/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ExamWeb_NguyenTanHoang.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
